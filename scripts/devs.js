2667fbbc8992330abfc37fbd2363e665ab55cc02
keepupwithpawan <pawankamatw@gmail.com>
2024-11-03 15:23:42 +0530

    Devs Socials

--- scripts/devs.js
+++ scripts/devs.js
@@ -22,3 +22,76 @@ contact.addEventListener('click', () => {
 devs.addEventListener('click', () => {
     location.href = 'devs.html';
 });
+
+// Pawan
+const linkedinPawan = document.getElementById("pawan-linkedin");
+const twitterPawan = document.getElementById("pawan-twitter");
+const instagramPawan = document.getElementById("pawan-instagram");
+const linkPawan = document.getElementById("pawan-link");
+
+linkedinPawan.addEventListener('click', () => {
+    location.href = 'https://www.linkedin.com/in/pawankamat/';
+});
+
+twitterPawan.addEventListener('click', () => {
+    location.href = 'https://x.com/keepupwithpa1';
+});
+
+instagramPawan.addEventListener('click', () => {
+    location.href = 'https://www.instagram.com/keepupwithpawan/';
+});
+
+linkPawan.addEventListener('click', () => {
+    location.href = 'https://pawan-kamat-portfolio.vercel.app/index.html';
+});
+
+// Aahana
+const linkedinAahana = document.getElementById("aahana-linkedin");
+const twitterAahana = document.getElementById("aahana-twitter");
+const instagramAahana = document.getElementById("aahana-instagram");
+
+linkedinAahana.addEventListener('click', () => {
+    location.href = 'https://www.linkedin.com/in/aahana-bobade/';
+});
+
+twitterAahana.addEventListener('click', () => {
+    location.href = 'https://x.com/BobadeAahana';
+});
+
+instagramAahana.addEventListener('click', () => {
+    location.href = 'https://www.instagram.com/aahanabobade1/';
+});
+
+// Shree
+const linkedinShree = document.getElementById("shridhar-linkedin");
+const twitterShree = document.getElementById("shridhar-twitter");
+const instagramShree = document.getElementById("shridhar-instagram");
+
+linkedinShree.addEventListener('click', () => {
+    location.href = 'https://www.linkedin.com/in/shridhar-iyer-94a526272/';
+});
+
+twitterShree.addEventListener('click', () => {
+    location.href = 'https://x.com/Shridhariyer45';
+});
+
+instagramShree.addEventListener('click', () => {
+    location.href = 'https://www.instagram.com/shree__iyer/';
+});
+
+// Alisha
+const linkedinAlisha = document.getElementById("alisha-linkedin");
+const twitterAlisha = document.getElementById("alisha-twitter");
+const instagramAlisha = document.getElementById("alisha-instagram");
+
+linkedinAlisha.addEventListener('click', () => {
+    location.href = 'https://www.linkedin.com/in/alisha-fernandes-541882297/';
+});
+
+twitterAlisha.addEventListener('click', () => {
+    location.href = '#';
+});
+
+instagramAlisha.addEventListener('click', () => {
+    location.href = 'https://www.instagram.com/alishaferns16/';
+});
